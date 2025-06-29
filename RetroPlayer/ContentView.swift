2891d935ac006b1eea98bbe36ccadb9b7b226f5c
conistuffwow <deatu013@gmail.com>
2025-06-29 12:56:19 -0400

    Initial Commit

---
+++ RetroPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetroPlayer
+//
+//  Created by Coni on 2025-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetroPlayer/RetroPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroPlayerApp.swift
+//  RetroPlayer
+//
+//  Created by Coni on 2025-06-29.
+//
+
+import SwiftUI
+
+@main
+struct RetroPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
